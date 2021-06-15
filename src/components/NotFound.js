5e1e53645f7e282417184d9309b719dcec2c1072
Milosz33 <milosz.skw@gmail.com>
2021-06-15 16:18:34 +0200

    create component Not Found

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+import "../scss/NotFound.scss"
+
+
+const NotFound = () => (
+
+    <div className="notfound-box">
+        <h1>404 - Not Found!</h1>
+        <NavLink to="/">
+            Go Home
+        </NavLink>
+    </div>
+);
+
+export default NotFound;
