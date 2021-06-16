f58ad421f0a75d78eff7bfaa94c2952040c386e2
Milosz33 <milosz.skw@gmail.com>
2021-06-16 13:43:54 +0200

    implementing NumField

---
+++ src/components/NumField.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { useFormikContext, useField } from "formik";
+import { urlNum } from "../assets/Urls";
+
+export const NumField = (props) => {
+    const {
+        values: { woj },
+        setFieldValue,
+    } = useFormikContext();
+    const [, meta] = useField(props);
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        async function fetchNum() {
+            console.log("Request: fetchNum");
+
+            const response = await fetch(urlNum, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify([
+                    {
+                        level: "woj",
+                        v: woj,
+                    },
+                    {
+                        level: "pow",
+                        q: "",
+                    },
+                    {
+                        level: "gmina",
+                        q: "",
+                    },
+                    {
+                        level: "city",
+                        q: "",
+                    },
+                    {
+                        level: "postCode",
+                        q: "",
+                    },
+                    {
+                        level: "num",
+                        q: "",
+                    },
+                ]),
+            });
+            const data = await response.json();
+            const options = data.map((item) => ({
+                value: item.value,
+                label: item.value,
+            }));
+
+            setOptions(options);
+        }
+
+        if (woj !== "") {
+            fetchNum();
+        }
+    }, [woj, props.name, setFieldValue]);
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setFieldValue(props.name, value);
+    };
+
+    return (
+        <>
+            <select name={props.name} onChange={handleChange}>
+                <option value="">wybierz numer</option>
+                {options.map((item, index) => (
+                    <option key={index} value={item.value}>
+                        {item.label}
+                    </option>
+                ))}
+            </select>
+            {!!meta.touched && !!meta.error && (
+                <div style={{ color: "red" }}>{meta.error}</div>
+            )}
+        </>
+    );
+};
